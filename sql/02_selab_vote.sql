b3d44de7601aa54ff24e02f7a44923ef7d1f1558
sunhongfei <021027shf@gmail.com>
2022-09-28 17:22:56 +0800

    Add enums and pojos

--- sql/02_selab_vote.sql
+++ sql/02_selab_vote.sql
@@ -18,30 +18,17 @@ insert into sys_menu
 values ('2004', '我的提议', '2000', '4', 'mine', 'vote/mine/index', '', 1, 1, 'C', '0', '0', 'vote:mine',
         'search', 'admin', sysdate(), '', null, '管理我的提议');
 
--- -- ----------------------------
--- -- 1、投票类型表
--- -- ----------------------------
--- drop table if exists selab_vote_type;
--- create table selab_vote_type
--- (
---     id             int         not null auto_increment comment '投票类型ID',
---     vote_type_name varchar(30) not null comment '投票类型名称',
---     remark         varchar(500) default '' comment '备注',
---     primary key (id)
--- ) engine=innodb comment = '投票类型表';
-
-
 -- ----------------------------
 -- 1、投票选项存储表
 -- ----------------------------
 drop table if exists selab_vote_option;
 create table selab_vote_option
 (
-    id           bigint      not null auto_increment comment '投票选项ID',
-    parent_id    bigint      not null comment '父选项',
-    vote_id      bigint      not null comment '所隶属于的投票id',
-    option_type  char(1)     not null comment '投票类型',
-    option_title varchar(20) not null comment '标题',
+    id          bigint      not null auto_increment comment '投票选项ID',
+    parent_id   bigint      not null comment '父选项',
+    vote_id     bigint      not null comment '所隶属于的投票id',
+    option_type char(1)     not null comment '投票类型',
+    content     varchar(30) not null comment '内容',
     primary key (id)
 ) engine=innodb comment = '投票详细信息表';
 
@@ -52,12 +39,12 @@ create table selab_vote_option
 drop table if exists selab_vote_info;
 create table selab_vote_info
 (
-    id           bigint   not null auto_increment comment '数据唯一标识',
-    user_id      bigint   not null comment '发起投票的用户ID',
-    vote_type_id int      not null comment '投票类型ID',
-    title        tinytext not null comment '投票标题',
-    content      text     not null comment '投票内容',
-    status       int      not null comment '状态',
+    id           bigint       not null auto_increment comment '数据唯一标识',
+    user_id      varchar(100) not null comment '发起投票的用户ID',
+    vote_type_id int          not null comment '投票类型ID',
+    title        tinytext     not null comment '投票标题',
+    content      text         not null comment '投票内容',
+    status       int          not null comment '状态',
     create_time  datetime comment '创建时间',
     update_time  datetime comment '更新时间',
     primary key (id)
@@ -66,14 +53,14 @@ create table selab_vote_info
 -- ----------------------------
 -- 3、投票结果表
 -- ----------------------------
-drop table if exists selab_vote_info;
-create table selab_vote_info
+drop table if exists selab_vote_result;
+create table selab_vote_result
 (
     id             bigint       not null auto_increment comment '数据唯一标识',
     vote_option_id bigint       not null comment '投票选项ID',
     user_id        varchar(100) not null comment '参与投票的用户ID（加密后）',
     content        text         not null comment '投票内容',
-    status         int          not null comment '状态',
+    isEnable       bool         not null comment '是否有效',
     create_time    datetime comment '创建时间',
     update_time    datetime comment '更新时间',
     primary key (id)

4a79135327d777657a4723358370d478c7a07b96
Steve-evens <3236805730@qq.com>
2022-10-04 10:55:21 +0800

    upload schedule controller

---
+++ selab-schedule/src/main/java/tyut/selab/schedule/controller/UploadController.java
@@ -0,0 +1,27 @@
+package tyut.selab.schedule.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import tyut.selab.schedule.domain.po.Schedule;
+import tyut.selab.schedule.service.UploadScheduleService;
+
+import java.util.List;
+
+/**
+ * @author Big_bai on 2022/10/4
+ */
+@Controller
+public class UploadController {
+    @Autowired
+    private UploadScheduleService uploadScheduleService;
+
+    @PostMapping("/selab/Schedule")
+    @PreAuthorize("@ss.hasAnyPermi('schedule:upload')")
+    public void uploadSchedule(@RequestBody List<Schedule> schedules){
+        uploadScheduleService.insertSchedule(schedules);
+    }
+
+}

5c3136f16865d3d1032ed0edce3ff533b01b5bdc
TokisakiKulumi <2105721136@qq.com>
2022-10-05 17:00:12 +0800

    fix Display Leisure function

--- selab-schedule/src/main/java/tyut/selab/schedule/service/IDisplayLeisureService.java
+++ selab-schedule/src/main/java/tyut/selab/schedule/service/IDisplayLeisureService.java
@@ -1,11 +1,13 @@
 package tyut.selab.schedule.service;
 
 import com.ruoyi.common.core.domain.entity.SysUser;
+import tyut.selab.schedule.domain.TimeFrame;
 import tyut.selab.schedule.domain.vo.DisplayLeisureRequest;
 import tyut.selab.schedule.domain.vo.DisplayLeisureResponse;
 
 import java.util.List;
+import java.util.Map;
 
 public interface IDisplayLeisureService {
-    List<DisplayLeisureResponse> getLeisure(DisplayLeisureRequest displayLeisureRequest);
+    Map<TimeFrame,List<DisplayLeisureResponse>> getLeisure(DisplayLeisureRequest displayLeisureRequest);
 }

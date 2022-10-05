fa23d2170a4adb50dc73ed2f7f105666db1e2351
Steve-evens <3236805730@qq.com>
2022-10-05 20:59:17 +0800

    fix service for uploadSchedule

--- selab-schedule/src/main/java/tyut/selab/schedule/service/impl/UploadScheduleService.java
+++ selab-schedule/src/main/java/tyut/selab/schedule/service/impl/UploadScheduleService.java
@@ -4,9 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tyut.selab.schedule.domain.po.Schedule;
 import tyut.selab.schedule.domain.vo.UploadScheduleRequest;
+import tyut.selab.schedule.enums.Period;
+import tyut.selab.schedule.enums.Status;
+import tyut.selab.schedule.enums.Week;
+import tyut.selab.schedule.enums.WeekNo;
 import tyut.selab.schedule.mapper.IUploadScheduleMapper;
 import tyut.selab.schedule.service.IUploadScheduleService;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -22,13 +28,25 @@ public class UploadScheduleService implements IUploadScheduleService {
     private IUploadScheduleMapper iUploadScheduleMapper;
 
     /**
-     * 天上传课表
+     * 上传课表
      * @param uploadScheduleRequests 课表集合
      * @param userId 用户id
      */
     @Override
     public void insertSchedule(List<UploadScheduleRequest> uploadScheduleRequests, Long userId){
-
+        List<Schedule> schedules = new ArrayList<>();
+        for(UploadScheduleRequest s:uploadScheduleRequests){
+            Schedule schedule = new Schedule();
+            schedule.setUserId(userId);
+            schedule.setCourseTitle(s.getCourseTitle());
+            schedule.setCreateTime(new Date());
+            schedule.setPeriod(s.getPeriod());
+            schedule.setWeek(s.getWeek());
+            schedule.setWeekNo(s.getWeekNo());
+            schedule.setStatus(Status.ENABLE);
+            schedules.add(schedule);
+        }
+        iUploadScheduleMapper.insertSchedule(schedules);
     }
 
 }

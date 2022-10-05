41d65d78f09b359411fd32d736629cd384da8c5f
Steve-evens <3236805730@qq.com>
2022-10-05 10:27:18 +0800

    UploadSchedule controller and service

--- selab-schedule/src/main/java/tyut/selab/schedule/controller/UploadController.java
+++ selab-schedule/src/main/java/tyut/selab/schedule/controller/UploadController.java
@@ -1,10 +1,12 @@
 package tyut.selab.schedule.controller;
 
+import com.ruoyi.common.core.controller.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tyut.selab.schedule.domain.po.Schedule;
 import tyut.selab.schedule.service.impl.UploadScheduleService;
@@ -15,14 +17,16 @@ import java.util.List;
  * @author Big_bai on 2022/10/4
  */
 @RestController
-public class UploadController {
+@RequestMapping("/selab/Schedule")
+public class UploadController extends BaseController {
     @Autowired
     private UploadScheduleService uploadScheduleService;
 
-    @PostMapping("/selab/Schedule")
+    @PostMapping("/add")
     @PreAuthorize("@ss.hasAnyPermi('schedule:mine:upload')")
     public void uploadSchedule(List<Schedule> schedules){
-        uploadScheduleService.insertSchedule(schedules);
+        uploadScheduleService.insertSchedule(schedules,getUserId());
+
     }
 
 }

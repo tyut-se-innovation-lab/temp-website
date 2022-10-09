2f456423517ebc674ad1eff153992f5e1c085324
TokisakiKulumi <2105721136@qq.com>
2022-10-09 20:31:10 +0800

    Fix URL

--- selab-schedule/src/main/java/tyut/selab/schedule/controller/MineScheduleController.java
+++ selab-schedule/src/main/java/tyut/selab/schedule/controller/MineScheduleController.java
@@ -3,13 +3,10 @@ package tyut.selab.schedule.controller;
 import com.ruoyi.common.core.controller.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import tyut.selab.schedule.domain.po.Schedule;
+import org.springframework.web.bind.annotation.*;
+import tyut.selab.schedule.domain.vo.ScheduleDisplayResponse;
 import tyut.selab.schedule.domain.vo.UploadScheduleRequest;
+import tyut.selab.schedule.service.IDisplayScheduleService;
 import tyut.selab.schedule.service.impl.UploadScheduleService;
 
 import java.util.List;
@@ -18,21 +15,30 @@ import java.util.List;
  * @author Big_bai on 2022/10/4
  */
 @RestController
-@RequestMapping("/selab/schedule")
-public class UploadController extends BaseController {
-    /**
-     * Service对象
-     */
+@RequestMapping("/selab/schedule/mine")
+public class MineScheduleController extends BaseController {
+
+    @Autowired
+    private IDisplayScheduleService displayScheduleService;
+
     @Autowired
     private UploadScheduleService uploadScheduleService;
+
 
     /**
      * 上传课表
      */
-    @PostMapping("/add")
+    @PostMapping("/upload")
     @PreAuthorize("@ss.hasAnyPermi('schedule:mine')")
     public void uploadSchedule(@RequestBody List<UploadScheduleRequest> uploadScheduleRequests) {
         uploadScheduleService.insertSchedule(uploadScheduleRequests, getUserId());
+    }
+
+    @GetMapping("/display")
+    @PreAuthorize("@ss.hasAnyPermi('schedule:mine')")
+    @ResponseBody
+    public List<ScheduleDisplayResponse> displaySchedule() {
+        return displayScheduleService.selectScheduleList(getUserId());
     }
 
 }

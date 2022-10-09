a8e54a3c3b333c1b23254ade7109155a8d3faab9
侯瑞宁 <94113626+xiaomougui@users.noreply.github.com>
2022-10-09 21:21:02 +0800

    Fix:the bug

--- ruoyi-ui/src/api/schedule/upload.js
+++ ruoyi-ui/src/api/schedule/upload.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 // 上传课表
 export function uploadSchedule(query) {
     return request({
-        url: '/selab/schedule/add',
+        url: '/selab/schedule/mine/upload',
         method: 'post',
         data: query
     })

b09d9c8d6a2541e25280e8d64b7a318fd00627e5
TokisakiKulumi <2105721136@qq.com>
2022-10-10 17:30:38 +0800

    Fix problem of dept

--- selab-schedule/src/main/java/tyut/selab/schedule/service/impl/DisplayLeisureServiceImpl.java
+++ selab-schedule/src/main/java/tyut/selab/schedule/service/impl/DisplayLeisureServiceImpl.java
@@ -1,5 +1,7 @@
 package tyut.selab.schedule.service.impl;
 
+import com.ruoyi.common.core.domain.entity.SysDept;
+import com.ruoyi.system.mapper.SysDeptMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tyut.selab.schedule.domain.TimeFrame;
@@ -8,9 +10,7 @@ import tyut.selab.schedule.domain.vo.DisplayLeisureResponse;
 import tyut.selab.schedule.mapper.DisplayLeisureMapper;
 import tyut.selab.schedule.service.IDisplayLeisureService;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class DisplayLeisureServiceImpl implements IDisplayLeisureService {
@@ -18,16 +18,27 @@ public class DisplayLeisureServiceImpl implements IDisplayLeisureService {
     @Autowired
     private DisplayLeisureMapper displayLeisureMapper;
 
+    @Autowired
+    private SysDeptMapper sysDeptMapper;
+
     public List<DisplayLeisureResponse> getLeisure(DisplayLeisureRequest displayLeisureRequest) {
         List<TimeFrame> timeFrames = displayLeisureRequest.getTimeFrames();
         List<Long> deptIds = displayLeisureRequest.getDeptIds();
+        List<Long> childDeptIds = new ArrayList<>();
         List<Long> roleIds = displayLeisureRequest.getRoleIds();
         List<Long> roleIdByDeptId = new ArrayList<>();
         List<Long> userIds = new LinkedList<>();
         List<DisplayLeisureResponse> response = new ArrayList<>();
 
         if (deptIds != null) {
-            roleIdByDeptId = displayLeisureMapper.getRoleIdsByDeptId(deptIds);
+            for (Long dept :deptIds ) {
+                List<SysDept> sysDepts = sysDeptMapper.selectChildrenDeptById(dept);
+                for (SysDept sysdept : sysDepts) {
+                    childDeptIds.add(sysdept.getDeptId());
+                }
+            }
+            childDeptIds.addAll(deptIds);
+            roleIdByDeptId = displayLeisureMapper.getRoleIdsByDeptId(childDeptIds);
         }
 
         if (roleIds != null) {
@@ -40,8 +51,8 @@ public class DisplayLeisureServiceImpl implements IDisplayLeisureService {
         if (roleIds != null) {
             userIds.addAll(displayLeisureMapper.getUserIdByRoleId(roleIds));
         }
-        if (deptIds != null) {
-            userIds.addAll(displayLeisureMapper.getUserIdByDeptId(deptIds));
+        if (!childDeptIds.isEmpty()) {
+            userIds.addAll(displayLeisureMapper.getUserIdByDeptId(childDeptIds));
         }
         if (timeFrames != null) {
             for (TimeFrame timeFrame : timeFrames) {
@@ -52,6 +63,18 @@ public class DisplayLeisureServiceImpl implements IDisplayLeisureService {
         }
         if (!userIds.isEmpty()) {
             response = displayLeisureMapper.getResponseByUserId(userIds);
+        }
+        if (deptIds !=null) {
+            System.out.println("deptIds"+Arrays.toString(deptIds.toArray()));
+        }
+        if (roleIds!=null) {
+            System.out.println("roleIds"+Arrays.toString(roleIds.toArray()));
+        }
+        if (timeFrames!=null) {
+            System.out.println("timeFrames"+Arrays.toString(timeFrames.toArray()));
+        }
+        if (userIds!=null) {
+            System.out.println("userIds"+Arrays.toString(userIds.toArray()));
         }
         return response;
 

bf4ccd5dcc79072890a0521d02a8c0d1af811060
xiguang <1480130605@qq.com>
2022-11-24 09:15:19 +0800

    Submit user voting results for implementation

---
+++ selab-vote/src/main/java/tyut/selab/vote/service/impl/CommitVoteServiceImpl.java
@@ -0,0 +1,29 @@
+package tyut.selab.vote.service.impl;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tyut.selab.vote.domain.po.VoteResult;
+import tyut.selab.vote.mapper.InsertInfoDBMapper;
+import tyut.selab.vote.service.ICommitVoteService;
+
+import java.util.List;
+
+/**
+ * @Author: Gulu
+ * @Date: 2022/11/23 18:46
+ */
+@Service
+public class CommitVoteServiceImpl implements ICommitVoteService {
+    @Autowired
+    InsertInfoDBMapper insertInfoDBMapper;
+
+    /**
+     * 提交用户投票结果
+     * @param results 用户投票结果集合
+     */
+    @Override
+    public int commitVoteResult(List<VoteResult> results) {
+        return insertInfoDBMapper.writeVoteResultToDB(results);
+    }
+}

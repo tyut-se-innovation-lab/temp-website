39549f79b7dff437a6d2b0a103a2a96403bf4897
xiguang <1480130605@qq.com>
2023-03-30 22:11:06 +0800

    Sign back data updates

---
+++ selab-attendance/src/main/java/tyut/selab/attendance/domain/po/CouldSignOut.java
@@ -0,0 +1,23 @@
+package tyut.selab.attendance.domain.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @Author: Gulu
+ * @Date: 2023/3/30 21:28
+ */
+@Data
+public class CouldSignOut {
+
+    /**
+     * 签到时间
+     */
+    Date attStartTime;
+
+    /**
+     * 是否可以签退
+     */
+    Boolean couldSignOut;
+}

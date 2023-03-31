d3dfff00ed33ba997c7c5e4646574e1c751d0d32
Steve-evens <3236805730@qq.com>
2023-03-31 12:47:46 +0800

    fix:attendance/log to attendance/signlog

--- selab-attendance/src/main/java/tyut/selab/attendance/service/Impl/AttendanceServiceImpl.java
+++ selab-attendance/src/main/java/tyut/selab/attendance/service/Impl/AttendanceServiceImpl.java
@@ -89,7 +89,6 @@ public class AttendanceServiceImpl implements IAttendanceService {
         Attendance attendance = attendanceMapper.couleSignOut(sysUserMapper.selectUserById(getUserId()).getNickName());
         Date attStartTime = attendance.getAttStartTime();
         Date endTime = attendance.getAttEndTime();
-
         if (attStartTime != null && endTime == null){
             Calendar cal1 = Calendar.getInstance();
             Calendar cal2 = Calendar.getInstance();

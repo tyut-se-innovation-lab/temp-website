18c5debd6dc88a30440971140015d7ca5acfad23
侯瑞宁 <94113626+xiaomougui@users.noreply.github.com>
2023-04-24 14:42:17 +0800

    to(vote):解决冲突

--- selab-vote/src/main/java/tyut/selab/vote/controller/LunchVoteController.java
+++ selab-vote/src/main/java/tyut/selab/vote/controller/LunchVoteController.java
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-package tyut.selab.vote.controller;
-
-import com.ruoyi.common.core.domain.AjaxResult;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import tyut.selab.vote.domain.vo.Questionnaire;
-import tyut.selab.vote.service.ILaunchVoteService;
-import tyut.selab.vote.tools.impl.VoPoConverterTool;
-
-import static com.ruoyi.common.utils.SecurityUtils.getUserId;
-
-/**
- * @author Big_bai on 2022/12/2
- */
-@RestController
-@RequestMapping("/selab/vote")
-public class LunchVoteController {
-
-    @Autowired
-    private ILaunchVoteService launchVoteService;
-
-    @PreAuthorize("@ss.hasPermi('vote:initiate')")
-    @PostMapping("/initiate")
-    public AjaxResult lunchVote(@RequestBody Questionnaire questionnaire){
-        launchVoteService.launchVote(questionnaire,getUserId());
-        return new AjaxResult(200,"投票正在上传,请稍后(⌓‿⌓)");
-    }
-}
-=======
 package tyut.selab.vote.controller;
 
 import com.ruoyi.common.core.domain.AjaxResult;
@@ -67,4 +33,3 @@ public class LunchVoteController {
         return new AjaxResult(200,"投票正在上传,请稍后(⌓‿⌓)");
     }
 }
->>>>>>> f19da3cf6c48cdcac67d21c49fdb43a0b78d8d99
--- selab-vote/src/main/java/tyut/selab/vote/domain/vo/Questionnaire.java
+++ selab-vote/src/main/java/tyut/selab/vote/domain/vo/Questionnaire.java
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-package tyut.selab.vote.domain.vo;
-
-import com.fasterxml.jackson.annotation.JsonSetter;
-import lombok.Data;
-import tyut.selab.vote.tools.GetSysTime;
-
-import java.util.Date;
-import java.util.List;
-
-/**
- * 问卷
- * @author Big_bai on 2022/11/19
- */
-@Data
-public class Questionnaire {
-    //id
-    private Long id;
-    //标题
-    private String title;
-    //简介
-    private String content;
-    //状态
-    private String status;
-    //开始时间
-    private Date creatTime;
-    //截止时间
-    private Date deadline;
-    //总人数
-    private int peoples;
-    //内容
-    private List<VoteQue> voteQues;
-    //是否支持撤回
-    private int authority;
-    //是否参与
-    private boolean isJoin;
-
-    @JsonSetter("context")
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    @JsonSetter("deadline")
-    public void setDeadline(String deadline) {
-        this.deadline= GetSysTime.stringToDate(deadline);
-    }
-
-    public void setDeadline(Date deadline) {
-        this.deadline = deadline;
-    }
-
-    @Override
-    public String toString() {
-        return "Questionnaire{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                ", status='" + status + '\'' +
-                ", creatTime=" + creatTime +
-                ", deadline=" + deadline +
-                ", peoples=" + peoples +
-                ", authority=" + authority +
-                ", isJoin=" + isJoin +
-                ", voteQues=" + voteQues +
-                '}';
-    }
-}
-=======
 package tyut.selab.vote.domain.vo;
 
 import com.fasterxml.jackson.annotation.JsonSetter;
@@ -136,4 +68,3 @@ public class Questionnaire {
                 '}';
     }
 }
->>>>>>> f19da3cf6c48cdcac67d21c49fdb43a0b78d8d99

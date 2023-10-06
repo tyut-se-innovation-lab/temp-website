74ffd49da369696ee56a78f6ed43bbc95e4f34e0
dell <3135631305@qq.com>
2023-10-06 17:38:32 +0800

    /

--- ruoyi-ui/src/views/login.vue
+++ ruoyi-ui/src/views/login.vue
@@ -72,8 +72,8 @@ export default {
     return {
       codeUrl: "",
       loginForm: {
-        username: "admin",
-        password: "admin123",
+        username: "武慧东",
+        password: "123456",
         rememberMe: false,
         code: "",
         uuid: ""
---
+++ ruoyi-ui/src/views/rule/announcement/index.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "rule"
+}
+</script>
+
+<template>
+  <div>123</div>
+</template>
+
+<style scoped >
+
+</style>

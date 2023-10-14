92ce2d256c489e427cb89090920f3aa5a99fb70a
lizhichao <2658085067@qq.com>
2023-10-14 22:46:10 +0800

    add mapper

---
+++ selab-vote/src/test/java/tyut/selab/vote/AppTest.java
@@ -0,0 +1,18 @@
+package tyut.selab.vote;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest {
+    @Test
+    public void showDateTime(){
+
+        System.out.println(new Date().getTime());
+    }
+
+}

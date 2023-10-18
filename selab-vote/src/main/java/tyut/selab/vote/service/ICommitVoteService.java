3c51f1a4d564c7a3405ff0ad213c005a06bb18d6
lizhichao <2658085067@qq.com>
2023-10-18 21:08:18 +0800

    fix: CommitVoteServiceImpl

--- selab-vote/src/main/java/tyut/selab/vote/service/ICommitVoteService.java
+++ selab-vote/src/main/java/tyut/selab/vote/service/ICommitVoteService.java
@@ -1,6 +1,7 @@
 package tyut.selab.vote.service;
 
 import org.springframework.stereotype.Service;
+import tyut.selab.vote.domain.DTO.VoteResultRequest;
 import tyut.selab.vote.domain.po.VoteInfo;
 import tyut.selab.vote.domain.po.VoteResult;
 import tyut.selab.vote.exception.VoteException;
@@ -12,9 +13,9 @@ public interface ICommitVoteService {
 
      /**
       *   提交投票信息
-      * @param voteResult
+      * @param voteResultRequest
       * @throws VoteException
       */
-     void commitVote(VoteResult voteResult) throws VoteException;
+     void commitVote(VoteResultRequest voteResultRequest) throws VoteException;
 
 }

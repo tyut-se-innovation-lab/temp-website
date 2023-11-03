0e1dda646d675ff50fb3cc1d1e2e4c61e30adf14
lizhichao <2658085067@qq.com>
2023-11-03 11:40:54 +0800

    fix:VoteOptionDetailsVo extends VoteOptionDTO

--- selab-vote/src/main/java/tyut/selab/vote/domain/vo/VoteOptionDetailsVo.java
+++ selab-vote/src/main/java/tyut/selab/vote/domain/vo/VoteOptionDetailsVo.java
@@ -1,6 +1,7 @@
 package tyut.selab.vote.domain.vo;
 
-import tyut.selab.vote.enums.VoteOptionType;
+import tyut.selab.vote.domain.DTO.VoteOptionDTO;
+import tyut.selab.vote.domain.po.VoteOpinionContent;
 
 import java.util.List;
 
@@ -11,69 +12,29 @@ import java.util.List;
  * @date: 2023/10/11 20:14
  * @version: 1.0
  */
-public class VoteOptionDetailsVo {
-    private Long id;
-    private Long voteId;
-    private VoteOptionType optionType;
-    private String content;
+public class VoteOptionDetailsVo extends VoteOptionDTO {
 
-    private String percentage;
-    private Integer chooseNum;
+
     private List<String> userNames;
+    private List<VoteOpinionContent> voteOpinionContents;
+
+
+
+    public List<String> getUserNames() {
+        return userNames;
+    }
 
     public VoteOptionDetailsVo() {
     }
 
-    public Long getId() {
-        return id;
+
+
+    public List<VoteOpinionContent> getVoteOpinionContents() {
+        return voteOpinionContents;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getVoteId() {
-        return voteId;
-    }
-
-    public void setVoteId(Long voteId) {
-        this.voteId = voteId;
-    }
-
-    public VoteOptionType getOptionType() {
-        return optionType;
-    }
-
-    public void setOptionType(VoteOptionType optionType) {
-        this.optionType = optionType;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getPercentage() {
-        return percentage;
-    }
-
-    public void setPercentage(String percentage) {
-        this.percentage = percentage;
-    }
-
-    public Integer getVoteNum() {
-        return chooseNum;
-    }
-
-    public void setVoteNum(Integer chooseNum) {
-        this.chooseNum = chooseNum;
-    }
-
-    public List<String> getUserName() {
-        return userNames;
+    public void setVoteOpinionContents(List<VoteOpinionContent> voteOpinionContents) {
+        this.voteOpinionContents = voteOpinionContents;
     }
 
     public void setUserNames(List<String> userNames) {
@@ -82,14 +43,9 @@ public class VoteOptionDetailsVo {
 
     @Override
     public String toString() {
-        return "VoteOptionDetails{" +
-                "id=" + id +
-                ", voteId=" + voteId +
-                ", optionType=" + optionType +
-                ", content='" + content + '\'' +
-                ", percentage=" + percentage +
-                ", voteNum=" + chooseNum +
-                ", userName=" + userNames +
+        return super.toString()+
+                "userNames=" + userNames +
+                ", voteOpinionContents=" + voteOpinionContents +
                 '}';
     }
 }

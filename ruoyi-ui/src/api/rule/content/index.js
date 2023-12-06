c67a09230c30e8395f0ca7b76d43750cde642fc5
dell <3135631305@qq.com>
2023-12-06 21:59:41 +0800

    修改content部分

--- ruoyi-ui/src/api/rule/content/index.js
+++ ruoyi-ui/src/api/rule/content/index.js
@@ -11,3 +11,15 @@ export const selectAllLog = (pageNum, pageSize) => {
 export const downLoad = () => {
   return request.get("/rule/file/download");
 };
+//上传规章制度的文件
+
+export function upLoad(file) {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  return request.post("/rule/file/upload", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+}
